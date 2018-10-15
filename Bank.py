5cb9aa06699b3dc2f3b48985dc9d097ecc9b915c
pythonaliens <43728293+pythonaliens@users.noreply.github.com>
2018-10-15 19:39:39 +0530

    Updated Bank.py
    Please implement separate objects for each customer.

--- Bank.py
+++ Bank.py
@@ -1,51 +1,103 @@
 import sys
-
-
 class Customer:
 
+    def __init__(self,username,accountnumber,password):
+        self.username = username
+        self.accountnumber = accountnumber
+        self.password = password
+    def signup(self):
+            print("Account successfully created",self.username)
+            print("Your Username is : ", self.username)
+            print("Your Accountnumber is : ", self.accountnumber)
+            print("Your password is : ",self.password)
 
-    def __init__(self, name, balance=0,Ubalance=0):
-        self.name = name
+
+class Account:
+
+    def __init__(self,balance=0,minamount=500,):
         self.balance = balance
-        self.Ubalance = Ubalance
+        self.minamount = minamount
+        self.firsttime = True
 
-    def deposit(self, amt):
-        if amt <= 500:
-            print("Deposit the Minimum Amount >= 500")
-        else:
-            self.balance = self.balance + amt
-            self.Ubalance = self.balance - 500
-            print("Mininum balance to be maintained = 500")
-            print("Current balance is ", self.Ubalance)
+    def deposit(self,amount):
+        if amount>0:
 
-    def withdraw(self, amt):
-        if amt > self.Ubalance:
-            print("Insufficient funds")
-            print("First deposit some amount")
+            if amount>=self.minamount or self.balance>=self.minamount:
+                    print(amount)
+                    self.balance=amount+self.balance
+                    print("sucessfully deposited amount : ",self.balance)
+            else:
+                print("Please Deposit Minimum Amount")
 
         else:
-            usable_balance = self.Ubalance - amt
-            print("Mininum balance maintaining 500")
-            print("After withdraw balance is ", usable_balance)
+            print("Negative amount not allowed")
+
+    def withDraw(self,amount):
+        if amount>0:
+                tempbalance = self.minamount
+                tempbalance=self.balance - amount
+                if(tempbalance>=500):
+                    self.balance = self.balance - amount
+                else:
+                    print("Ensufficient Funds to remove", amount)
+
+        else:
+            print("Negitive Amount Not Allowed")
+
+    def checkBalance(self):
+        print("current balance is :",self.balance)
+
+    def exit(self):
+        sys.exit()
+bankdetails = Account()
+account = Account()
+class MainProgram:
+
+    while True:
+        print(" 1-SignUp\n 2-SignIn")
+        option = int(input("Choose your option"))
+        if option == 1:
+            username = input("Enter the username")
+            accountnumber = input("Please enter account number")
+            password = input("Please choose any password")
+            amount = int(input("Enter the initial amount"))
+            account.deposit(amount)
+            if amount>=500:
+                customerclass = Customer(username, accountnumber, password)
+                customerclass.signup()
+            else:
+                print("account creation failed sign up again")
+
+        if option == 2:
+            loop = True
+            while loop:
+                print("1-Enter your password\n 2: Exit")
+                option = int(input("Enter your option"))
+                if option ==1:
+                    password = input("Please Enter your password")
+                    if password =="acc":
+                        while True:
+                            print(" 1-Deposit\n 2-Withdraw\n 3-CheckBalance\n 4-Logout")
+                            option = int(input("Choose any one of the options"))
+                            if option == 1:
+                                amount = int(input("Enter the amount to Deposit"))
+                                account2.deposit(amount)
+                            if option == 2:
+                                amount = int(input("Enter the amount to Withdraw"))
+                                account.withDraw(amount)
+                            if option == 3:
+                                account.checkBalance()
+                            if option == 4:
+                                break
+                    loop = False
 
 
-bankname = 'Durgasoft'
-print("welcome to "+ bankname +" bank")
-name = input("Enter your name :: ")
-c = Customer(name)
 
-while True:
-    print(" d-Deposit\n w-Withdraw\n e-Exit")
-    option = input("choose your option")
-    if option.lower() == 'd':
-        amt = float(input("Enter the amount to deposit"))
-        c.deposit(amt)
-    elif option.lower() == 'w':
-        amt = float(input("Enter the amount to withdraw"))
-        c.withdraw(amt)
 
-    elif option.lower() == 'e':
-        print("Thanks for banking")
-        break
-    else:
-        print("choose valid option")
+
+
+
+
+
+
+
